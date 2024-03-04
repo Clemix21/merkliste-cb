aabfc1aafa42b242bd44e3f3552808e8d0da02a2
Clemens <92445409+Clemix21@users.noreply.github.com>
2024-03-04 14:04:05 +0100

    implemented check for existing database table

--- src/model/Database.php
+++ src/model/Database.php
@@ -7,13 +7,28 @@ class Database
     private $password = 'password123';
     private $dbname = 'merkliste';
 
-    public function getConnection()
-    {
+    public function getConnection() {
         $dsn = 'mysql:dbname=merkliste;host=mysql';
         $conn = new PDO($dsn, $this->user, $this->password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        // check ob eine tabelle namens notes existiert
+        if (!$this->doesTableExist($conn, 'notes')) {
+            $this->initializeDatabase($conn);
+        }
+
         return $conn;
+    }
+
+    private function doesTableExist($conn, $tableName) {
+        $stmt = $conn->query("SHOW TABLES LIKE '$tableName'");
+        return $stmt->rowCount() > 0;
+    }
+
+    private function initializeDatabase($conn) {
+        $sqlFilePath = __DIR__ . '/../data/notes.sql';
+        $sql = file_get_contents($sqlFilePath);
+        $conn->exec($sql);
     }
 }
 
